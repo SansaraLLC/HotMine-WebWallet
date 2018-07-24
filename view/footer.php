8c6aa76dd83788a006fe4f59189ec9991d1b71d6
Alex Collin <a@sansara.me>
2018-07-24 10:17:08 +0300

    gi

--- view/footer.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined("IN_WALLET")) { die("Auth Error!."); } ?>
-            </div>
-        </div>
-
-    </body>
-<b><center><p>Powered by <a href="https://sansara.me">Sansara</a></p>
-</center></b>
-</html>
